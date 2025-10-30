302570535b5ba5aaf6bd51f2f73ca64654b67d18
Ivan Hernandez <ihernandez2018@my.fit.edu>
2025-10-29 23:20:13 -0400

    added rank add and sub

--- discord-bot/src/services/ss_service.py
+++ discord-bot/src/services/ss_service.py
@@ -9,3 +9,15 @@ class StarSystem:
         async with self.pool.acquire() as conn:
             return await conn.fetch("SELECT student_name, ss_ranking FROM users \
                                     WHERE affiliation = $1 ORDER BY ss_ranking DESC LIMIT $2;",house,limit)
+
+    async def get_student_rank(self, house: str, student: str):
+        async with self.pool.acquire() as conn:
+            return await conn.fetchrow("SELECT ss_ranking FROM users \
+                                    WHERE affiliation = $1 AND student_name = $2;",house,student)
+        
+    async def set_student_rank(self, house: str, student: str, quantity: int):
+        async with self.pool.acquire() as conn:
+            return await conn.fetch("UPDATE users \
+                                    SET ss_ranking = $1 \
+                                    WHERE affiliation = $2 AND student_name = $3 \
+                                    RETURNING student_name, ss_ranking;",quantity,house,student)
