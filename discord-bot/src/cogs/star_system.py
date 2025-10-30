bf44296a3e1d249f3efc1e9790590577afc619f1
Ivan Hernandez <ihernandez2018@my.fit.edu>
2025-10-29 23:20:53 -0400

    added frontend for add/sub feature, as well as added permission handler

--- discord-bot/src/cogs/star_system.py
+++ discord-bot/src/cogs/star_system.py
@@ -30,15 +30,21 @@ class starSystemCog(commands.Cog):
                 pass
         self._groups.clear()
 
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, commands.MissingRole):
+            await ctx.send("❌ You don’t have permission to use that command.")
+
     def _make_group(self, cfg):
         house = cfg["name"]
+        gCmd = house.lower()
         service = self.system
 
         async def _entry(ctx: commands.Context):
             await ctx.send(f"**{house}** commands: try `!{house} top`")
 
 
-        grp = commands.Group(_entry,name=house.lower(), help=f"{house} commands",invoke_without_command=True)
+        grp = commands.Group(_entry,name=gCmd, help=f"{house} commands",invoke_without_command=True)
 
         @grp.command(name="top", help=f"Show the top rankers for house {house}.")
         async def top(ctx):
@@ -48,6 +54,43 @@ class starSystemCog(commands.Cog):
                 return
             msg = "\n".join(f"{i+1}. {r['student_name']} — {r['ss_ranking']}" for i, r in enumerate(rows))
             await ctx.send(f"🏆 **Top Ranker for house {house} **\n{msg}")
+
+        @commands.has_role("SS-Admin")
+        @grp.command(name="add", help=f"Add stars to members of House {house}.")
+        async def add(ctx, _student: str = None, _num: int = None):
+            if not _student or not _num:
+                await ctx.send(f"⚠️ You must provide a name and amount! Usage: `!{gCmd} add <student_name> <quantity> `")
+                return
+
+            row = await service.get_student_rank(house,_student)
+            if not row:
+                await ctx.send(f"Could not find {_student} in House {house}.")
+                return
+            _new = row['ss_ranking'] + _num
+            rows = await service.set_student_rank(house,_student,_new)
+            if not rows:
+                await ctx.send(f"Failed add {_num} stars to {_student}.")
+                return
+            await ctx.send(f"Congratulations {_student}! you've earn {_num} stars bring you up to {_new}!")
+
+        @commands.has_role("SS-Admin")
+        @grp.command(name="sub", help=f"Subtract stars to members of House {house}.")
+        async def sub(ctx, _student: str = None, _num: int = None):
+            if not _student or not _num:
+                await ctx.send(f"⚠️ You must provide a name and amount! Usage: `!{gCmd} sub <student_name> <quantity> `")
+                return
+
+            row = await service.get_student_rank(house,_student)
+            if not row:
+                await ctx.send(f"Could not find {_student} in House {house}.")
+                return
+            _new = row['ss_ranking'] - _num
+            rows = await service.set_student_rank(house,_student,_new)
+            if not rows:
+                await ctx.send(f"Failed add {_num} stars to {_student}.")
+                return
+            await ctx.send(f"Damn that had to hurt... {_student} lost {_num} stars, leaving them at {_new}... oof...")
+
 
         return grp
 
